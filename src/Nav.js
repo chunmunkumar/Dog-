28c9d26024f17bf1a358cd3fe2c6710f935c435e
chunmunkumar <92389819+chunmunkumar@users.noreply.github.com>
2022-02-10 22:41:05 +0530

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Nav() {
+  return <div className='navbar'>
+      <h1>
+          Dog Gallary
+      </h1>
+      <p>suggation</p>
+  </div>;
+}
